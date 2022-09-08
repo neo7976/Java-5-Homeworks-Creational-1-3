323cdc3d085350fe2d5b4857aa385b444eab8a3b
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-08 19:09:05 +0300

    Шаблон Фабрики по пончикам

--- src/main/java/DoughnutFactory.java
+++ src/main/java/DoughnutFactory.java
@@ -1,23 +1,35 @@
 import doughnut.*;
 
 public class DoughnutFactory {
+    private int cherryDoughnutCount = 0;
+    private int almondDoughnutCount = 0;
+    private int chocolateDoughnutCount = 0;
+
     public Doughnut getDoughnut(DoughnutTypes type) {
         Doughnut toReturn = null;
         switch (type) {
             case ALMOND -> {
                 toReturn = new AlmondDoughnut();
-                break;
+                almondDoughnutCount++;
             }
             case CHERRY -> {
                 toReturn = new CherryDoughnut();
-                break;
+                cherryDoughnutCount++;
             }
             case CHOCOLATE -> {
                 toReturn = new ChocolateDoughnut();
-                break;
+                chocolateDoughnutCount++;
             }
             default -> throw new IllegalArgumentException("Ошибочный ввод доната: " + type);
         }
         return toReturn;
+    }
+
+    public void printCount() {
+        System.out.println("Посчитаем съеденные пончики?");
+        System.out.printf("Мы съели пончик с начинкой {%s} в количестве: %10s %3d шт.\n", DoughnutTypes.ALMOND, "", almondDoughnutCount);
+        System.out.printf("Мы съели пончик с начинкой {%s} в количестве: %10s %3d шт.\n", DoughnutTypes.CHERRY, "", cherryDoughnutCount);
+        System.out.printf("Мы съели пончик с начинкой {%s} в количестве: %7s %3d шт.\n", DoughnutTypes.CHOCOLATE, "", chocolateDoughnutCount);
+        System.out.printf("Всего было съедено пончиков: %30s %4d шт.\n", "", almondDoughnutCount + cherryDoughnutCount + chocolateDoughnutCount);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import doughnut.Doughnut;
 import doughnut.DoughnutTypes;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         DoughnutFactory factory = new DoughnutFactory();
@@ -12,5 +14,23 @@ public class Main {
         cherry.eat();
         chocolate.eat();
         almond.eat();
+        for (int i = 0; i < 100; i++) {
+            eatRandomDoughnut(factory);
+        }
+        factory.printCount();
+
     }
+
+    public static Doughnut getRandomDoughnut(DoughnutFactory factory) {
+        Random random = new Random();
+        DoughnutTypes type = DoughnutTypes.values()[random.nextInt(DoughnutTypes.values().length)];
+        return factory.getDoughnut(type);
+    }
+
+    public static void eatRandomDoughnut(DoughnutFactory factory) {
+        Doughnut randomDoughnut = getRandomDoughnut(factory);
+        System.out.println("Сюрприз!");
+        randomDoughnut.eat();
+    }
+
 }
