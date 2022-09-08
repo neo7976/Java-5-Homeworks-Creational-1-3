2e0dca14069333adf7d2a54cbad0045e6eb06623
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-08 18:43:03 +0300

    Первые написания Шаблона Фабрики по пончикам

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import doughnut.Doughnut;
+import doughnut.DoughnutTypes;
+
+public class Main {
+    public static void main(String[] args) {
+        DoughnutFactory factory = new DoughnutFactory();
+
+        Doughnut cherry = factory.getDoughnut(DoughnutTypes.CHERRY);
+        Doughnut chocolate = factory.getDoughnut(DoughnutTypes.CHOCOLATE);
+        Doughnut almond = factory.getDoughnut(DoughnutTypes.ALMOND);
+
+        cherry.eat();
+        chocolate.eat();
+        almond.eat();
+    }
+}
